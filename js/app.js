b6d8b8ee3c2b11ba65cb3ac394dd44040fcec758
Arvib Fazriansyah <85182275+arv-fazriansyah@users.noreply.github.com>
2024-02-15 10:43:30 +0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -701,15 +701,15 @@ const comment = (() => {
         const DIV = document.createElement('div');
         DIV.classList.add('mb-3');
         DIV.innerHTML = `
-        <div class="card-body bg-light shadow p-3 m-0 rounded-4" data-parent="true" id="${data.uuid}">
+        <div class="card-body border bg-transparent shadow p-3 m-0 rounded-4" data-parent="true" id="${data.uuid}">
             <div class="d-flex flex-wrap justify-content-between align-items-center">
-                <p class="text-dark text-truncate m-0 p-0" style="font-size: 0.95rem;">
+                <p class="text-light text-truncate m-0 p-0" style="font-size: 0.95rem;">
                     <strong class="me-1">${util.escapeHtml(data.nama)}</strong><i class="fa-solid ${data.hadir ? 'fa-circle-check text-success' : 'fa-circle-xmark text-danger'}"></i>
                 </p>
-                <small class="text-dark m-0 p-0" style="font-size: 0.75rem;">${data.created_at}</small>
+                <small class="text-light m-0 p-0" style="font-size: 0.75rem;">${data.created_at}</small>
             </div>
-            <hr class="text-dark my-1">
-            <p class="text-dark mt-0 mb-1 mx-0 p-0" style="white-space: pre-line">${convertMarkdownToHTML(util.escapeHtml(data.komentar))}</p>
+            <hr class="text-light my-1">
+            <p class="text-light mt-0 mb-1 mx-0 p-0" style="white-space: pre-line">${convertMarkdownToHTML(util.escapeHtml(data.komentar))}</p>
             ${innerComment(data)}
         </div>`;
         return DIV;
